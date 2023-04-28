15324876909b0ef015a263f67213f16efa31ce6b
Vinicius-G-R <125162352+Vinicius-G-R@users.noreply.github.com>
2023-04-28 18:11:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿ bool movendo;
+  bool deveria;
+    
+  Console.WriteLine("--- Fluxograma da Engenharia ---\n");
+
+  Console.Write("O objeto está se movendo (S/N)? ");
+    movendo = Console.ReadLine().ToUpper() == "S";
+    
+Console.Write("Deveria? ");    
+    deveria = Console.ReadLine().ToUpper() == "S";
+    
+if (movendo && deveria)
+{
+  Console.ForegroundColor = ConsoleColor.Green;
+  Console.WriteLine("\nÓtimo.");
+  Console.ResetColor();
+}
+    
+else if (movendo && !deveria)
+{
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine("\nUse Silver Tape.");  
+  Console.ResetColor();
+}
+    
+else if (!movendo && deveria)
+{
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nUse WD-40.");
+Console.ResetColor();
+}
+    
+else
+{
+  Console.ForegroundColor = ConsoleColor.Green;
+  Console.WriteLine("\nÓtimo.");
+  Console.ResetColor();
+}
+      
